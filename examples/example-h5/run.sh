f3dfae386c005ad8434dc5aaa6292c5a3a50ae47
Leandro Balmaceda <balmacedalm@gmail.com>
2018-12-02 20:52:39 -0300

    Update examples to match new cli path

--- examples/example-h5/run.sh
+++ examples/example-h5/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ../../evaluator/evaluator.py -a ../examples-utils/result.h5 -u ../examples-utils/OVP-dataset-UTN.h5 -v ../examples-utils/v60.mpg --user_summary_path v60/user_summary --automatic_summary_path v60/machine_summary -m cus
+python3 ../../ost-python/ost/evaluator_cli.py -a ../examples-utils/result.h5 -u ../examples-utils/OVP-dataset-UTN.h5 -v ../examples-utils/v60.mpg --user_summary_path v60/user_summary --automatic_summary_path v60/machine_summary -m cus
 echo ""
-python3 ../../evaluator/evaluator.py -a ../examples-utils/result.h5 -u ../examples-utils/OVP-dataset-UTN.h5 -v ../examples-utils/v60.mpg --user_summary_path v60/user_summary --automatic_summary_path v60/machine_summary -m bhi
+python3 ../../ost-python/ost/evaluator_cli.py -a ../examples-utils/result.h5 -u ../examples-utils/OVP-dataset-UTN.h5 -v ../examples-utils/v60.mpg --user_summary_path v60/user_summary --automatic_summary_path v60/machine_summary -m bhi
--- examples/same-data/run.sh
+++ examples/same-data/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ../../evaluator/evaluator.py -a data/ -u reference/ -v ../examples-utils/v60.mpg -m cus
+python3 ../../ost-python/ost/evaluator_cli.py -a data/ -u reference/ -v ../examples-utils/v60.mpg -m cus
 echo ""
-python3 ../../evaluator/evaluator.py -a data/ -u reference/ -v ../examples-utils/v60.mpg -m bhi
+python3 ../../ost-python/ost/evaluator_cli.py -a data/ -u reference/ -v ../examples-utils/v60.mpg -m bhi
